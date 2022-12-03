fcbdb01ca180fa7b219884eff913e48885533583
Stunxzz <106486287+Stunxzz@users.noreply.github.com>
2022-12-04 00:28:46 +0200

    contacts-menu
    виж какво можеш още да добавиш към менюто , вкарал съм fontawesome

--- solarSystem.js
+++ solarSystem.js
@@ -1,6 +1,9 @@
 const solarEl = document.getElementById("solar-system")
 const solarBtn = document.getElementById("solar-btn")
 const homeEl = document.getElementById("home")
+const contactsBtn = document.getElementById("contacts-link")
+const contactsMenu = document.getElementById("contacts")
+const closeIcon = document.getElementById("cross-icon")
 
 solarBtn.addEventListener("click",()=>{
     solarEl.style.display="flex";
@@ -9,4 +12,13 @@ solarBtn.addEventListener("click",()=>{
 
 homeEl.addEventListener("click",()=>{
     solarEl.style.display ="none";
+})
+
+contactsBtn.addEventListener("click",()=>{
+    contactsMenu.style.display="inline";
+
+})
+
+closeIcon.addEventListener("click",()=>{
+    contactsMenu.style.display="none";
 })
